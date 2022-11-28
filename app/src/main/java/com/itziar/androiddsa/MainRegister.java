c8b722bc7929e56fb63cd300816c984551124169
Itziar Mensa <itziar.mensa@estudiantat.upc.edu>
2022-11-28 13:00:10 +0100

    Design done

---
+++ app/src/main/java/com/itziar/androiddsa/MainRegister.java
@@ -0,0 +1,14 @@
+package com.itziar.androiddsa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_main);
+    }
+}
